e4f5433f32fe4b97a8a5bb22187cf1da92c07b31
Thomas Dimperio <twdimperio@gmail.com>
2022-08-30 14:28:44 -0600

    Debug index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 
 export default function App() {
