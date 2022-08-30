e46521e651a6382e6bae82ff5c5688c4e7ee4f33
Thomas Dimperio <twdimperio@gmail.com>
2022-08-30 14:26:20 -0600

    Initial frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from "./components/SignUp";
+
+export default function App() {
+
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <SignUp />
+    </Router>
+  );
+}
