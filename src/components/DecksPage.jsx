7f3466be214c3593af00acadf0da001280006fff
Thomas Dimperio <twdimperio@gmail.com>
2022-11-08 13:44:29 -0700

    Add link styling to index.css and DecksPage

--- src/components/DecksPage.jsx
+++ src/components/DecksPage.jsx
@@ -6,9 +6,9 @@ const DecksPage = ({ decks }) => {
   return (
     <div>
       {decks.map(deck => (
-      <Link to={`/decks/${deck.id}`} key={deck.id}>{deck.name}</Link>
+      <Link to={`/decks/${deck.id}`} className="link" key={deck.id}>{deck.name}</Link>
     ))}
-      <Link to={"/decks/new"}>New Deck</Link>
+      <Link to={"/decks/new"} className="link">New Deck</Link>
     </div>
   )
 }
